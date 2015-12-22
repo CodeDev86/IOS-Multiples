5f106f2cb34fcfa7e78cd5e9c240eec30a0d5f2d
David McCaslin <david.mccaslin@outlook.com>
2015-12-21 22:37:07 -0700

    First Pass at the Multiples App

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var bannerImg: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var deskImg: UIImageView!
+    @IBOutlet weak var multipleTxt: UITextField!
+    
+    @IBOutlet weak var chalkBoardImg: UIImageView!
+    @IBOutlet weak var currentMultiple: UILabel!
+    @IBOutlet weak var mutipleBtn: UIButton!
+    
+    var multiple:Int? = nil
+    var nextMultiple:Int = 0
+    let maxMultiple:Int = 10
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func PlayBtnPressed(sender: UIButton) {
+        
+        multiple = Int(multipleTxt.text!)
+        
+        if(multiple != nil) {
+            multiple = Int(multipleTxt.text!)!
+            NextMultiple()
+        
+            HideHomeScreen(true)
+            HideGameScreen(false)
+        }
     }
-
+    
+    @IBAction func MultipleBtnPressed(sender: UIButton) {
+        if(nextMultiple <= 10) {
+            NextMultiple()
+        } else {
+            ResetGame()
+        }
+    }
+    
+    func NextMultiple(){
+        currentMultiple.text = "\(multiple!) X \(nextMultiple) = " + "\(multiple! * nextMultiple)"
+        nextMultiple++
+    }
+    
+    func ResetGame() {
+        HideGameScreen(true)
+        HideHomeScreen(false)
+        
+        multipleTxt.text = ""
+        currentMultiple.text = ""
+        multiple = nil
+        nextMultiple = 0
+    }
+    
+    func HideHomeScreen(isHidden:Bool) {
+        bannerImg.hidden = isHidden
+        playBtn.hidden = isHidden
+        deskImg.hidden = isHidden
+        multipleTxt.hidden = isHidden
+    }
+    
+    func HideGameScreen(isHidden:Bool) {
+        chalkBoardImg.hidden = isHidden
+        currentMultiple.hidden = isHidden
+        mutipleBtn.hidden = isHidden
+    }
 
 }
 
